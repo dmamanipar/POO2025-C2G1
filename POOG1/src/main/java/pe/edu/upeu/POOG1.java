9c758955e4cc1323e1629ee40a36f54d974c8810
dmamanipar <dmamanipar@unsa.edu.pe>
2025-08-15 15:42:54 -0500

    Primer proyecto

---
+++ POOG1/src/main/java/pe/edu/upeu/POOG1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pe.edu.upeu;
+
+/**
+ *
+ * @author SOFTWARE
+ */
+public class POOG1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
